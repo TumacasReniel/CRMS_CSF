f6f1b83e10598c125cbb4d8d997b7b76864eaa9d
idontwindows <63185758+idontwindows@users.noreply.github.com>
2024-04-24 15:14:58 +0800

    updated css 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @routes
         {{-- @vite(['resources/js/app.js', 'resources/css/app.css', "resources/js/Pages/{$page['component']}.vue"]) --}}
-        @vite(['resources/js/app.js'],'resources/css/app.css',)
+        @vite(['resources/js/app.js','resources/css/app.css'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
