1063a72197bb8be8319f102b3fe31dc8afea1c8d
Reniel Tumacas <TumacasReniel>
2024-06-07 13:38:37 +0800

    hide login

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -37,11 +37,11 @@ defineProps({
                             Get Started
                         </v-btn>
                     </a>
-                    <a :href="route('login')">
+                    <!-- <a :href="route('login')">
                         <button class="mr-3 ml-5 text-blue-800 bg-gray-200 hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Login
                         </button>
-                    </a>
+                    </a> -->
                 
                 <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
