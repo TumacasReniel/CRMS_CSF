cb51362d147f11d265b59c33109da7cef77861de
TS-OneLab-6 <TumacasReniel>
2025-01-30 09:35:22 +0800

    back dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,7 +13,7 @@ import { Head, Link, router } from '@inertiajs/vue3';
             </h2>
         </template>
 
-        <v-row class="mx-15 mt-5" >
+        <!-- <v-row class="mx-15 mt-5" >
             <v-card class="overflow-visible mb-5" >   
                           <v-divider class="border-opacity-100"></v-divider>
                           <v-row class="p-3 overflow-visible" >
@@ -178,7 +178,7 @@ import { Head, Link, router } from '@inertiajs/vue3';
                                               outlined="none"> 
                                         </v-select>
                                   </v-col>   
-
+                              </v-row>
         
                               </v-card-body>
                     </v-card>
@@ -316,9 +316,67 @@ import { Head, Link, router } from '@inertiajs/vue3';
                     </v-card>
                 </Link>
             </v-col>
-        </v-row>
+        </v-row> -->
         
+        <v-row class="mx-15 mt-5">
+                <Link href="/accounts">
+                    <div class="py-5 ml-5 mr-5 " style="width:250px">
+                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <v-icon size="x-large" class="p-3" >mdi-account</v-icon>
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                    Accounts
+                                </h5>
+                            </a>  
 
+                        </div>
+                    </div>     
+                 </Link>
+            
+
+                <Link href="/service_units">
+                    <div class="py-5 ml-5 mr-5 " style="width:250px">
+                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <v-icon size="x-large" class="p-3" >mdi-domain</v-icon>
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                    Services Units
+                                </h5>
+                            </a>  
+
+                        </div>
+                    </div>     
+                </Link>
+
+                    <Link href="/regions">
+                    <div class="py-5 ml-5 mr-5 " style="width:250px">
+                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <v-icon size="x-large" class="p-3" >mdi-map-marker</v-icon>
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                    Regions
+                                </h5>
+                            </a>  
+
+                        </div>
+                    </div>     
+                </Link>
+
+                   <Link href="/pstos">
+                    <div class="py-5 ml-5 mr-5 " style="width:250px">
+                        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <v-icon size="x-large" class="p-3" >mdi-home-map-marker</v-icon>
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                    PSTOs
+                                </h5>
+                            </a>  
+
+                        </div>
+                    </div>     
+                </Link>
+         
+        </v-row>
 
      
 
