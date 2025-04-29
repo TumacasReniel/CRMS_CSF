4753438bb76cf42215852668f6c1f53c0cc83606
darpagadian <dar.pagadian@gmail.com>
2025-04-29 15:51:16 +0800

    update app title

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 // import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName = import.meta.env.VITE_APP_NAME || 'CSF';
+const appName = import.meta.env.VITE_APP_NAME || 'CRMS';
 
 createInertiaApp({
     title: (title) => title ? `${appName} | ${title}` : appName,
